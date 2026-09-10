79ac2454584510daf8d0b7a85b21db8dc7b01d72
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-10 11:07:52 -0400

    Set local time.

--- scripts/current-time.js
+++ scripts/current-time.js
@@ -1,6 +1,19 @@
+
+
+//plugins for time and time zone
 dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 
+function currentTime(){
+    const hrs = document.getElementById("hrs");
+    const mins = document.getElementById("mins");
+    const secs = document.getElementById("secs");
 
-function time(){
-    console.log(dayjs().format('MMMM d YYYY'))
+    hrs.textContent = dayjs().format("hh");
+    mins.textContent = dayjs().format("mm");
+    secs.textContent = dayjs().format("ss A");
+
 }
+
+setInterval(currentTime, 1000);
+currentTime();
