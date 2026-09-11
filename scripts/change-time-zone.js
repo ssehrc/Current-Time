3d0cb0004f8c241c44a1d14b05e7c468783d7624
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-09-11 11:53:36 -0400

    Created options for user to change timezone.

--- scripts/change-time-zone.js
+++ scripts/change-time-zone.js
@@ -1,4 +1,21 @@
 import MicroModal from "micromodal";
 
 MicroModal.init();
-MicroModal.show('modal-1');
+
+//get timezone button
+const timezoneButton = document.getElementById("open-timezone-window");
+
+timezoneButton.addEventListener("click", function(){
+    MicroModal.show("modal-1");
+});
+
+//will handle timezone selection
+document.querySelectorAll("timezone-option").forEach(button => {
+    button.addEventListener("click", function(){
+        const selectedTimezone = this.getAttribute("data-timezone");
+
+        //current-time.js will use this
+        window.selectedTimezone = selectedTimezone;
+        MicroModal.close("modal-1");
+    });
+});
